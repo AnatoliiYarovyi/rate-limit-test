b62b84e7e75cb45d2986c21e571c66fc38ab5649
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-16 18:46:59 +0300

    Update Keys.ts

--- src/controllers/Keys.ts
+++ src/controllers/Keys.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { TypedDataResponse } from '../interfaces/Controllers';
+import { ApiKeysCache } from '../interfaces/App';
 
 export class KeysCtrl {
 	async getKeys(req: Request, res: Response, next: NextFunction) {
 		try {
-			const qwe = req.app.locals.apiKeysCache as string;
+			const qwe = req.app.locals.apiKeysCache as ApiKeysCache[];
 
-			const typedDataResponse: TypedDataResponse<string> = {
-				message: qwe,
+			const typedDataResponse: TypedDataResponse<ApiKeysCache[]> = {
+				results: qwe,
 			};
 
 			res.status(200).json({
