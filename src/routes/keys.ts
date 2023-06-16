fb4696fff2a0f001d152e3aa035e146e79cef755
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-16 20:51:10 +0300

    added middleware dynamicRateLimiter

--- src/routes/keys.ts
+++ src/routes/keys.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 
+import dynamicRateLimiter from '../middlewares/dynamicRateLimiter';
 import { KeysCtrl } from '../controllers/Keys';
 
 const router = express.Router();
 const keysCtrl = new KeysCtrl();
 
-router.get('/', (req, res, next) => keysCtrl.getKeys(req, res, next));
+router.get(
+	'/',
+	dynamicRateLimiter({
+		windowMs: 1 * 60 * 1000, // 1 min
+		max: (req) => req.app.locals.apiKeysCache[0].limit,
+		standardHeaders: true,
+		legacyHeaders: false,
+	}),
+	(req, res, next) => keysCtrl.getKeys(req, res, next)
+);
 
 export default router;
