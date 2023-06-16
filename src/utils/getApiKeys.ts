68af0c3e57a258c9fc892166b421cc0780eaa859
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-16 18:47:13 +0300

    Create getApiKeys.ts

---
+++ src/utils/getApiKeys.ts
@@ -0,0 +1,34 @@
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const { REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
+
+const dynamoDBClient = new DynamoDBClient({
+	region: REGION,
+	credentials: {
+		accessKeyId: AWS_ACCESS_KEY_ID,
+		secretAccessKey: AWS_SECRET_ACCESS_KEY,
+	},
+});
+
+export const getApiKeys = async () => {
+	try {
+		const params = {
+			TableName: 'api-keys-test',
+		};
+		const command = new ScanCommand(params);
+		const response = await dynamoDBClient.send(command);
+
+		const apiKeys = response.Items.map((item) => {
+			return {
+				id: item.id.S,
+				apiKey: item.apiKey.S,
+				limit: +item.limit.N,
+			};
+		});
+		return apiKeys;
+	} catch (error) {
+		console.error('getApiKeys:', error);
+	}
+};
