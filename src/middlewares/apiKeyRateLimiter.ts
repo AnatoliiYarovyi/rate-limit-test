03262f6fca2846f125b5b9116ad391e7d6732b4c
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-26 18:52:33 +0300

    Update apiKeyRateLimiter.ts

--- src/middlewares/apiKeyRateLimiter.ts
+++ src/middlewares/apiKeyRateLimiter.ts
@@ -9,11 +9,21 @@ const apiKeyRateLimiter = (options: { windowMs: number }) => {
 	const apiLimiter = rateLimit({
 		windowMs: options.windowMs,
 		max: getMaxValue,
-		standardHeaders: true,
+		keyGenerator: (req: Request) =>
+			(req.query.apiKey as string) ||
+			(req.headers['x-forwarded-for'] as string) ||
+			req.socket.remoteAddress,
+		standardHeaders: true, 
 		legacyHeaders: false,
+		message: 'Too many requests, please try again later. [ apiKeyRateLimiter ]',
 	});
 
 	function getMaxValue(req: Request) {
+		console.log('request.ip', req.ip);
+		console.log(`req.headers['x-forwarded-for']`, req.headers['x-forwarded-for']);
+		console.log('req.socket.remoteAddress', req.socket.remoteAddress);
+		
+		
 		const { apiKey } = req.query;
 		const limit = apiKey
 			? req.app.locals.apiKeysCache.reduce((acc: number, el: ApiKeysCache) => {
@@ -32,7 +42,10 @@ const apiKeyRateLimiter = (options: { windowMs: number }) => {
 		try {
 			apiLimiter(req, res, next);
 		} catch (error) {
-			res.status(429).json({ message: 'Too many requests, please try again later.' });
+			res.status(429).json({
+				message:
+					'Too many requests, please try again later. [ catch -> apiKeyRateLimiter ]',
+			});
 		}
 	};
 };
