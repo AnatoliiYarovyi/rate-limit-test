e979015f78eeadda1954b22f75afb74f2dc04393
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-26 18:52:35 +0300

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -3,17 +3,20 @@ import { Request, Response, NextFunction } from 'express';
 
 const rateLimiter = () => {
 	const apiLimiter = rateLimit({
-		windowMs: 3 * 1000 /* 1s */,
-		max: 5,
+		windowMs: 1 * 1000 /* 5s */,
+		max: 9,
 		standardHeaders: true,
 		legacyHeaders: false,
+		message: 'Too many requests, please try again later. [ rateLimiter ]',
 	});
 
 	return (req: Request, res: Response, next: NextFunction) => {
 		try {
 			apiLimiter(req, res, next);
 		} catch (error) {
-			res.status(429).json({ message: 'Too many requests, please try again later.' }); 
+			res.status(429).json({
+				message: 'Too many requests, please try again later. [ catch -> rateLimiter ]',
+			}); 
 		}
 	};
 };
