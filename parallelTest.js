9244a8b17296bb12c3ffb264b3d1076592abbbdf
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-06-27 17:38:22 +0300

    Update parallelTest.js

--- parallelTest.js
+++ parallelTest.js
@@ -1,48 +1,31 @@
-const http = require('http');
+const axios = require('axios');
 
-function makeRequest(url) {
-	return new Promise((resolve, reject) => {
-		const request = http.get(url, (response) => {
-			let data = '';
-			response.on('data', (chunk) => {
-				data += chunk;
-			});
-
-			response.on('end', () => {
-				if (response.statusCode >= 200 && response.statusCode < 300) {
-					const jsonData = JSON.parse(data);
-					resolve(jsonData);
-				} else {
-					reject(
-						new Error(`Request to ${url} failed with status ${response.statusCode}`)
-					);
-				}
-			});
-		});
-
-		request.on('error', (error) => {
-			reject(new Error(`Error making request to ${url}: ${error.message}`));
-		});
-	});
+async function makeRequest(url) {
+	try {
+		const response = await axios.get(url);
+		return response.data;
+	} catch (error) {
+		throw new Error(`Error making request to ${url}: ${error.message}`);
+	}
 }
 
 async function makeParallelRequests() {
 	try {
 		const urls = [
-			'http://localhost:3000/apiKey?apiKey=2',
-			'http://localhost:3000/apiKey?apiKey=2',
-			'http://localhost:3000/apiKey?apiKey=2',
+			'http://localhost:3000/noApiKey',
+			'http://localhost:3000/noApiKey',
+			'http://localhost:3000/noApiKey',
+			'http://localhost:3000/noApiKey',
 		];
 
 		const requests = urls.map((url) => makeRequest(url));
 		const responses = await Promise.allSettled(requests);
-		console.log('requests', requests);
 
 		responses.forEach((response, index) => {
 			if (response.status === 'fulfilled' && response.value) {
-				console.log(`Request ${index + 1} succeeded:`, response.value);
+				console.log(`✅ Request ${index + 1} succeeded:`, response.value);
 			} else {
-				console.log(`Request ${index + 1} failed:`, response);
+				console.log(`❌ Request ${index + 1} failed:`, response.value);
 			}
 		});
 	} catch (error) {
